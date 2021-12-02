36bb6ef607a76e274f5b342c765b0f30163d3b55
basverweij <basverweij@users.noreply.github.com>
2021-12-02 19:21:20 +0100

    feat: complete day 1, puzzle 1

---
+++ src/AdventOfCode2021.Day01/Program.cs
@@ -0,0 +1,15 @@
+﻿var lines = await File.ReadAllLinesAsync("input.txt");
+
+var measurements = lines.Select(int.Parse).ToArray();
+
+var solution1 = 0;
+
+for (var i = 1; i < measurements.Length; i++)
+{
+    if (measurements[i] > measurements[i - 1])
+    {
+        solution1++;
+    }
+}
+
+Console.WriteLine($"Day 1 - Puzzle 1: {solution1}");
