d7e960ea37490d9c4235496835ea7cfac5e4ec1a
basverweij <basverweij@users.noreply.github.com>
2021-12-02 19:29:21 +0100

    feat: complete day 1, puzzle 2

--- src/AdventOfCode2021.Day01/Program.cs
+++ src/AdventOfCode2021.Day01/Program.cs
@@ -13,3 +13,15 @@ for (var i = 1; i < measurements.Length; i++)
 }
 
 Console.WriteLine($"Day 1 - Puzzle 1: {solution1}");
+
+var solution2 = 0;
+
+for (var i = 1; i < measurements.Length - 2; i++)
+{
+    if (measurements[i..(i + 3)].Sum() > measurements[(i - 1)..(i + 2)].Sum())
+    {
+        solution2++;
+    }
+}
+
+Console.WriteLine($"Day 1 - Puzzle 2: {solution2}");
