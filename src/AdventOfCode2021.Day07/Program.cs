f2df154803842a0b8111efb48b93761dc8dbd5c4
basverweij <basverweij@users.noreply.github.com>
2021-12-11 17:26:21 +0100

    feat: complete day 7, puzzle 1

---
+++ src/AdventOfCode2021.Day07/Program.cs
@@ -0,0 +1,19 @@
+﻿var input = await File.ReadAllTextAsync("input.txt");
+
+var crabs = input.Split(',').Select(int.Parse).ToArray();
+
+var max = crabs.Max();
+
+var solution1 = int.MaxValue;
+
+for (var i = 0; i < max; i++)
+{
+    var fuel = crabs.Sum(c => Math.Abs(c - i));
+
+    if (fuel < solution1)
+    {
+        solution1 = fuel;
+    }
+}
+
+Console.WriteLine($"Day 7 - Puzzle 1: {solution1}");
