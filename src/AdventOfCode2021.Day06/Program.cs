2224a5fe5b2d875caef34469105fa2eee019a2f7
basverweij <basverweij@users.noreply.github.com>
2021-12-11 16:07:38 +0100

    feat: complete day 6, puzzle 1

---
+++ src/AdventOfCode2021.Day06/Program.cs
@@ -0,0 +1,26 @@
+﻿var input = await File.ReadAllTextAsync("input.txt");
+
+var fish = input.Split(',').Select(int.Parse).ToList();
+
+for (var i = 0; i < 80; i++)
+{
+    var n = fish.Count;
+
+    for (var j = 0; j < n; j++)
+    {
+        if (fish[j] == 0)
+        {
+            fish[j] = 6;
+
+            fish.Add(8);
+        }
+        else
+        {
+            fish[j]--;
+        }
+    }
+}
+
+var solution1 = fish.Count;
+
+Console.WriteLine($"Day 6 - Puzzle 1: {solution1}");
