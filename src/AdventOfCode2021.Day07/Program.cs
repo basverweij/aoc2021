2f6c05a1c196d5737978e8673c45c76cf48cb323
basverweij <basverweij@users.noreply.github.com>
2021-12-11 17:35:36 +0100

    feat: complete day 7, puzzle 2

--- src/AdventOfCode2021.Day07/Program.cs
+++ src/AdventOfCode2021.Day07/Program.cs
@@ -2,18 +2,31 @@
 
 var crabs = input.Split(',').Select(int.Parse).ToArray();
 
-var max = crabs.Max();
-
-var solution1 = int.MaxValue;
-
-for (var i = 0; i < max; i++)
-{
-    var fuel = crabs.Sum(c => Math.Abs(c - i));
-
-    if (fuel < solution1)
-    {
-        solution1 = fuel;
-    }
-}
+var solution1 = GetMinFuel(crabs, i => i);
 
 Console.WriteLine($"Day 7 - Puzzle 1: {solution1}");
+
+var solution2 = GetMinFuel(crabs, i => (i * i + i) / 2);
+
+Console.WriteLine($"Day 7 - Puzzle 2: {solution2}");
+
+int GetMinFuel(
+    int[] crabs,
+    Func<int, int> cost)
+{
+    var max = crabs.Max();
+
+    var minFuel = int.MaxValue;
+
+    for (var i = 0; i < max; i++)
+    {
+        var fuel = crabs.Sum(c => cost(Math.Abs(c - i)));
+
+        if (fuel < minFuel)
+        {
+            minFuel = fuel;
+        }
+    }
+
+    return minFuel;
+}
