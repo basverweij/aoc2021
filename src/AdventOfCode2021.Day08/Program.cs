1f520de18e19d7edb147c159f8935ad41617f31e
basverweij <basverweij@users.noreply.github.com>
2021-12-12 07:35:59 +0100

    feat: complete day 8, puzzle 1

---
+++ src/AdventOfCode2021.Day08/Program.cs
@@ -0,0 +1,19 @@
+﻿var input = await File.ReadAllLinesAsync("input.txt");
+
+var displays = input.Select(ParseDisplay).ToArray();
+
+var solution1 = displays.Sum(display => display.outputDigits.Count(digit => digit.Length is 2 or 3 or 4 or 7));
+
+Console.WriteLine($"Day 8 - Puzzle 1: {solution1}");
+
+(string[] signalPatterns, string[] outputDigits) ParseDisplay(
+    string input)
+{
+    var parts = input.Split(" | ");
+
+    var signalPatterns = parts[0].Split(' ').ToArray();
+
+    var outputDigits = parts[1].Split(' ').ToArray();
+
+    return (signalPatterns, outputDigits);
+}
