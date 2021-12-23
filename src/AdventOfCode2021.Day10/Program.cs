4bffdb1c0fc5c8b92bf399719248eb7718463cbf
basverweij <basverweij@users.noreply.github.com>
2021-12-23 12:06:57 +0100

    feat: complete day 10, puzzle 2

--- src/AdventOfCode2021.Day10/Program.cs
+++ src/AdventOfCode2021.Day10/Program.cs
@@ -1,10 +1,18 @@
 ﻿var input = await File.ReadAllLinesAsync("input.txt");
 
-var solution1 = input.Select(GetSyntaxErrorScore).Sum();
+var scores = input.Select(GetLineScores).ToArray();
+
+var solution1 = scores.Sum(s => s.syntaxError);
 
 Console.WriteLine($"Day 10 - Puzzle 1: {solution1}");
 
-static int GetSyntaxErrorScore(
+var incomplete = scores.Where(s => s.syntaxError == 0).OrderBy(s => s.autoComplete).ToArray();
+
+var solution2 = incomplete[incomplete.Length / 2].autoComplete;
+
+Console.WriteLine($"Day 10 - Puzzle 2: {solution2}");
+
+static (int syntaxError, long autoComplete) GetLineScores(
     string line)
 {
     var syntax = new Dictionary<char, char>
@@ -15,12 +23,20 @@ static int GetSyntaxErrorScore(
         ['<'] = '>',
     };
 
-    var scores = new Dictionary<char, int>
+    var syntaxErrorScores = new Dictionary<char, int>
     {
         [')'] = 3,
         [']'] = 57,
         ['}'] = 1197,
         ['>'] = 25137,
+    };
+
+    var autoCompleteScores = new Dictionary<char, long>
+    {
+        [')'] = 1,
+        [']'] = 2,
+        ['}'] = 3,
+        ['>'] = 4,
     };
 
     var chunks = new Stack<char>();
@@ -37,9 +53,13 @@ static int GetSyntaxErrorScore(
         if (!chunks.Any() ||
             c != chunks.Pop())
         {
-            return scores[c];
+            return (syntaxErrorScores[c], 0);
         }
     }
 
-    return 0;
+    var autoComplete = chunks.Aggregate(
+        0L,
+        (a, b) => a * 5 + autoCompleteScores[b]);
+
+    return (0, autoComplete);
 }
