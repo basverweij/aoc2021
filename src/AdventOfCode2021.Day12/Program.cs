cb3f901dccfa512f4fe59d55b6c5550548ef15e3
basverweij <basverweij@users.noreply.github.com>
2021-12-23 14:14:46 +0100

    feat: complete day 12, puzzle 2

--- src/AdventOfCode2021.Day12/Program.cs
+++ src/AdventOfCode2021.Day12/Program.cs
@@ -6,6 +6,10 @@ var solution1 = FindPaths(graph, new() { "start" });
 
 Console.WriteLine($"Day 12 - Puzzle 1: {solution1}");
 
+var solution2 = FindPaths(graph, new() { "start" }, 1);
+
+Console.WriteLine($"Day 12 - Puzzle 2: {solution2}");
+
 static Dictionary<string, HashSet<string>> BuildGraph(
     string[] input)
 {
@@ -42,12 +46,20 @@ static void UpdateGraph(
 
 static int FindPaths(
     Dictionary<string, HashSet<string>> graph,
-    List<string> path)
+    List<string> path,
+    int allowedDuplicateSmallCaves = 0)
 {
     var count = 0;
 
     foreach (var link in graph[path.Last()])
     {
+        if (link == "start")
+        {
+            // not allowed to go back to the start cave
+
+            continue;
+        }
+
         if (link == "end")
         {
             // found path to end
@@ -58,15 +70,27 @@ static int FindPaths(
         }
 
         if ((link.ToLowerInvariant() == link) &&
-            path.Contains(link))
+            path.Contains(link) &&
+            DuplicateSmallCaves(path) >= allowedDuplicateSmallCaves)
         {
-            // small caves can only be visited once
+            // small caves can only be visited once, and
+            // the path can contain up to allowedDuplicateSmallCaves
+            // duplicates
 
             continue;
         }
 
-        count += FindPaths(graph, new(path) { link });
+        count += FindPaths(graph, new(path) { link }, allowedDuplicateSmallCaves);
     }
 
     return count;
+}
+
+static int DuplicateSmallCaves(
+    IEnumerable<string> path)
+{
+    return path
+        .Where(n => n.ToLowerInvariant() == n)
+        .GroupBy(n => n)
+        .Count(g => g.Count() > 1);
 }
