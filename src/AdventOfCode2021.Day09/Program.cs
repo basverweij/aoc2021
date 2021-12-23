863b15e134967df9c9ecf3cbf785c1e0073b53a9
basverweij <basverweij@users.noreply.github.com>
2021-12-23 11:23:30 +0100

    feat: complete day 9, puzzle 2

--- src/AdventOfCode2021.Day09/Program.cs
+++ src/AdventOfCode2021.Day09/Program.cs
@@ -8,6 +8,14 @@ var solution1 = lowPoints.Select(p => area[p.y][p.x] + 1).Sum();
 
 Console.WriteLine($"Day 9 - Puzzle 1: {solution1}");
 
+var solution2 = lowPoints
+    .Select(p => GetBasinSize(area, p.x, p.y))
+    .OrderByDescending(s => s)
+    .Take(3)
+    .Aggregate(1, (a, b) => a * b);
+
+Console.WriteLine($"Day 9 - Puzzle 2: {solution2}");
+
 static IEnumerable<(int x, int y)> GetLowPoints(
     int[][] area)
 {
@@ -35,4 +43,57 @@ static bool IsLowPoint(
         (y == area.Length - 1 || height < area[y + 1][x]) && // bottom
         (x == 0 || height < area[y][x - 1]) && // left
         (x == area[y].Length - 1 || height < area[y][x + 1]); // right
+}
+
+static int GetBasinSize(
+    int[][] area,
+    int x,
+    int y)
+{
+    var basin = new HashSet<(int x, int y)>();
+
+    FillBasin(basin, area, x, y);
+
+    return basin.Count;
+}
+
+static void FillBasin(
+    HashSet<(int x, int y)> basin,
+    int[][] area,
+    int x,
+    int y)
+{
+    var height = area[y][x];
+
+    if (height == 9)
+    {
+        return;
+    }
+
+    if (basin.Contains((x, y)))
+    {
+        return;
+    }
+
+    basin.Add((x, y));
+
+    if (y > 0 && height < area[y - 1][x])
+    {
+        FillBasin(basin, area, x, y - 1); // top
+    }
+
+    if (y < area.Length - 1 && height < area[y + 1][x])
+    {
+        FillBasin(basin, area, x, y + 1); // bottom
+    }
+
+    if (x > 0 && height < area[y][x - 1])
+    {
+        FillBasin(basin, area, x - 1, y); // left
+    }
+
+    if (x < area[y].Length - 1 && height < area[y][x + 1])
+    {
+        FillBasin(basin, area, x + 1, y); // right
+    }
 }
