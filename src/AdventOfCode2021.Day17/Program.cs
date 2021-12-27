b5cb266e8d1929aead47fd31aab3e12579c70d6f
basverweij <basverweij@users.noreply.github.com>
2021-12-27 13:02:08 +0100

    feat: complete day 17, puzzle 2

--- src/AdventOfCode2021.Day17/Program.cs
+++ src/AdventOfCode2021.Day17/Program.cs
@@ -8,36 +8,50 @@ var targetY = (min: parts[2], max: parts[3]);
 
 var solution1 = int.MinValue;
 
-for (var y = 1; y <= targetX.max; y++)
+var solution2 = 0;
+
+for (var y = targetY.min; y <= targetX.max; y++)
 {
-    if (CanLaunch(y, targetX, targetY, out var maxY) &&
-        maxY > solution1)
+    if (CanLaunch(y, targetX, targetY, out var maxY, out var options))
     {
-        solution1 = maxY;
+        if (maxY > solution1)
+        {
+            solution1 = maxY;
+        }
+
+        solution2 += options;
     }
 }
 
 Console.WriteLine($"Day 17 - Puzzle 1: {solution1}");
 
+Console.WriteLine($"Day 17 - Puzzle 2: {solution2}");
+
 bool CanLaunch(
     int yVelocity,
     (int min, int max) targetX,
     (int min, int max) targetY,
-    out int maxY)
+    out int maxY,
+    out int options)
 {
     maxY = int.MinValue;
+
+    options = 0;
 
     for (var xVelocity = 1; xVelocity <= targetX.max; xVelocity++)
     {
         if (SimulateLaunch(yVelocity, xVelocity, targetX, targetY, out var simulationMaxY))
         {
-            maxY = simulationMaxY;
+            if (simulationMaxY > maxY)
+            {
+                maxY = simulationMaxY;
+            }
 
-            return true;
+            options++;
         }
     }
 
-    return false;
+    return options != 0;
 }
 
 bool SimulateLaunch(
